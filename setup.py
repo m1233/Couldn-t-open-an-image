d5149596cbc6e8fcb5d11354dd609555aacba177
m1233 <38630937+m1233@users.noreply.github.com>
2019-08-06 19:42:59 +0300

    Delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-from cx_Freeze import setup, Executable
-
-setup(name="Mygame",
-      version="1.0",
-      description="my game",
-      options={"build.exe":{"packages":["pygame"],
-                            "included_files":["vivi.png","anastasia.png","Bird.png"]}},
-      executables=[Executable("george_abc.py")])
